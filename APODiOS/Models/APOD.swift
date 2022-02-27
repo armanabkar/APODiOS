26f02844eb1acc1a116c8a7a2cdd79c3c05089d3
Arman Abkar <armanabkar@gmail.com>
2022-02-27 21:03:53 +0330

    add networking

---
+++ APODiOS/Models/APOD.swift
@@ -0,0 +1,16 @@
+//
+//  APOD.swift
+//  APOD
+//
+//  Created by Arman Abkar on 2/26/22.
+//
+
+import Foundation
+
+struct APOD: Codable {
+    let title: String?
+    let copyright: String?
+    let explanation: String?
+    let media_type: String?
+    let url: String?
+}
---
+++ APODiOS/Models/WebService.swift
@@ -0,0 +1,40 @@
+//
+//  WebService.swift
+//  APOD
+//
+//  Created by Arman Abkar on 2/26/22.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badURL
+    case noData
+    case decodingError
+}
+
+protocol API {
+    func fetchAPOD(for date: String) async throws -> APOD
+}
+
+final class WebService: API {
+    
+    static let shared = WebService()
+    private init() {}
+    private var cache = NSCache<AnyObject, AnyObject>()
+
+    func fetchAPOD(for date: String) async throws -> APOD {
+        guard let url = URL(string: "https://api.nasa.gov/planetary/apod?date=\(date)&api_key=DEMO_KEY") else { throw NetworkError.badURL }
+        
+        let urlRequest = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw NetworkError.noData }
+        
+        guard let decodedAPOD = try? JSONDecoder().decode(APOD.self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return decodedAPOD
+    }
+    
+}
